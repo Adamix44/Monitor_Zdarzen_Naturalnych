8ad623cc5a2271b5517b12518c5a2693df2e05f1
Adam_Rybacki <rybackiadam44@gmail.com>
2026-05-02 16:47:58 +0200

    Krok 2c: Repozytorium zarzadzajace danymi w tle

--- app/src/main/java/com/example/monitorzdarzennaturalnych/data/network/EonetApiService.kt
+++ app/src/main/java/com/example/monitorzdarzennaturalnych/data/network/EonetApiService.kt
@@ -6,10 +6,6 @@ import retrofit2.http.Query
 
 // interfejs sieciowy do pobierania danych NASA
 interface EonetApiService {
-    
     // cel API dla aktywnych wydarzen
-    @GET("events")
-    suspend fun getEvents(
-        @Query("status") status: String = "open"
-    ): EventResponse
+    @GET("events") suspend fun getEvents(@Query("status") status: String = "open"): EventResponse
 }
---
+++ app/src/main/java/com/example/monitorzdarzennaturalnych/repository/EventRepository.kt
@@ -0,0 +1,26 @@
+package com.example.monitorzdarzennaturalnych.repository
+
+import com.example.monitorzdarzennaturalnych.data.model.Event
+import com.example.monitorzdarzennaturalnych.data.network.RetrofitInstance
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+
+// posrednik umozliwiajacy bezpieczne laczenie z NASA
+class EventRepository {
+    
+    // asynchroniczna funkcja dzialajaca w tle
+    suspend fun getEvents(): List<Event> {
+        
+        // wydelegowanie do narzedzi zapis/odczyt (IO) by nie zatrzymac ekranu
+        return withContext(Dispatchers.IO) { 
+            try {
+                // proba sciagniecia listy z wygenerowanego zlacza Retrofit
+                val response = RetrofitInstance.api.getEvents()
+                response.events
+            } catch (e: Exception) {
+                // w razie braku sieci, przekazujemy pusta liste i unikamy bledu
+                emptyList()
+            }
+        }
+    }
+}
