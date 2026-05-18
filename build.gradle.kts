fb86067279fe6219bcaa79c880d122dcd645f146
Adam2604 <adammichala11@gmail.com>
2026-05-18 14:18:56 +0200

    Dodanie ikonki aplikacji

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@
 //dodajemy opcje konfiguracyjne wspolne dla calej aplikacji
 plugins {
     //instalator glownej architektury Androida
-    id("com.android.application") version "9.1.1" apply false
+    id("com.android.application") version "9.2.1" apply false
     //dodatek pozwalajacy traktowac kod w "Kotlin" jako jezyk systemu Android
     id("org.jetbrains.kotlin.android") version "2.2.10" apply false
     //biblioteka Google pozwalajaca aplikacji ukrywac Klucz API z local.properties
